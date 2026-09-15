27d4ca4d47967a2863975af84956d6c73e687f1e
Nicklas Bregenborg Sivertsen <nicklas_sivertsen@hotmail.com>
2026-09-15 13:24:27 +0200

    added click function to the heart icon

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const wrapperDom = document.querySelector("#wrapper")
 const bodyDom = document.querySelector("body")
-console.log(wrapperDom)
 
 async function airbnbData(airBnb) {
     try {
@@ -12,6 +11,7 @@ async function airbnbData(airBnb) {
         const destinationsData = await response.json()
         destinationsData.destinations.forEach(destination => {
             htmlDom(destination)
+            favouriteChoice(destination)
         });
 
     } catch (error) {
@@ -21,8 +21,8 @@ async function airbnbData(airBnb) {
 
 airbnbData()
 
-function rent(){
-    bodyDom.insertAdjacentHTML("afterbegin", 
+function rent() {
+    bodyDom.insertAdjacentHTML("afterbegin",
         /* HTML */
         `
         <h1 id="rent">Apartments for rent</h1>
@@ -37,9 +37,38 @@ function htmlDom(destination) {
         `
         <a href="destination.html?id=${destination.id}"><div class="location">
         <img src="./img/${destination.image}" alt="${destination.title}">
-        <div class="details"><i class="fa-regular fa-heart"></i> <p class="read_more">MORE</p></div>
+        <div class="details"><i class="fa-regular fa-heart" data-fav="${destination.id}"></i> <p class="read_more">MORE</p></div>
         </div></a>
         `
     )
-    
+
+
+}
+
+function favouriteChoice(favourite) {
+    const fav = document.querySelector(`[data-fav="${favourite.id}"]`)
+
+    let favourites = JSON.parse(localStorage.getItem("favourites")) || []
+
+    if (favourites.includes(favourite.id)) {
+        fav.classList.add("favourite")
+    }
+    fav.addEventListener("click", (favouriteChosen) => {
+    favouriteChosen.preventDefault()
+    favouriteChosen.stopPropagation()
+
+    let favourites = JSON.parse(localStorage.getItem("favourites")) || []
+
+    if (favourites.includes(favourite.id)) {
+        favourites = favourites.filter(id => id !== favourite.id)
+        fav.classList.remove("favourite")
+    } else {
+        favourites.push(favourite.id)
+        fav.classList.add("favourite")
+    }
+
+    localStorage.setItem("favourites", JSON.stringify(favourites))
+
+    console.log(favourites)
+})        
 }
